344c177185098e149881545944e5e5e7d964f507
Robin Goussey <robin.goussey@ugent.be>
2019-11-29 14:02:34 +0100

    WIP: error

--- bash_scripts/run_mysqlcontainer.sh
+++ bash_scripts/run_mysqlcontainer.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-docker network create test_network || true
+#docker network create test_network || true
 docker rm mySQLdb || true
-docker run --name=mySQLdb --network=test_network -p 3306:3306 -d --env="MYSQL_ROOT_PASSWORD=ThePassword" --env="MYSQL_DATABASE=Day"  mysql:5.7
+docker run --name mySQLdb -p 3306:3306 --env "MYSQL_ROOT_PASSWORD=ThePassword" --env "MYSQL_DATABASE=Day" -d mysql:5.7
