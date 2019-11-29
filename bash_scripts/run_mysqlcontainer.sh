ed12bd960fce2304a11d51964ebe574666c9526b
Robin Goussey <robin.goussey@ugent.be>
2019-11-29 12:45:42 +0100

    WIP before branch

--- bash_scripts/run_mysqlcontainer.sh
+++ bash_scripts/run_mysqlcontainer.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
-docker run --name=mySQLdb -p 3306:3306 -d --env="MYSQL_ROOT_PASSWORD=ThePassword" --env="MYSQL_DATABASE=Day"  mysql:5.7
+docker network create test_network || true
+docker rm mySQLdb || true
+docker run --name=mySQLdb --network=test_network -p 3306:3306 -d --env="MYSQL_ROOT_PASSWORD=ThePassword" --env="MYSQL_DATABASE=Day"  mysql:5.7
---
+++ bash_scripts/run_planningcontainer.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+docker run hall_planning --network=test_network -p 2223
+
