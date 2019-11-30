a9e061dedf84761c69e42adfc430a4109102f2a1
Gertjan De Mulder <gertjan.demulder@ugent.be>
2019-11-30 23:25:55 +0100

    init MFS

---
+++ movie_flow_service/src/main/java/system_design/project/MovieFlowServiceApplication.java
@@ -0,0 +1,42 @@
+package system_design.project;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
+
+
+@SpringBootApplication
+public class MovieFlowServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieFlowServiceApplication.class, args);
+	}
+	
+	@Bean
+	CommandLineRunner sayHello() {
+		
+		return (args)->{
+			final Logger logger = LoggerFactory.getLogger(MovieFlowServiceApplication.class);
+			System.out.println("sayHello....");
+			logger.info("logger says hello...");
+			RestTemplate restTemplate  = new RestTemplate();
+			String planningUrl = "http://localhost:2223/planning";
+			ResponseEntity<String> response = restTemplate.getForEntity(planningUrl, String.class);
+			
+			logger.info("response: " + response);
+			
+		};
+	}
+	
+	
+
+}
