35ca9c22444afc72cbed85f81e91ba34deec7341
Gertjan De Mulder <gertjan.demulder@ugent.be>
2019-12-01 19:43:18 +0100

    added @DateTimeFormat annotation

--- hall_planning_service/src/main/java/system_design/project/hall_planning_service/adapters/rest/PlanningRestController.java
+++ hall_planning_service/src/main/java/system_design/project/hall_planning_service/adapters/rest/PlanningRestController.java
@@ -6,6 +6,7 @@ import java.util.List;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -35,7 +36,7 @@ public class PlanningRestController {
 	}
 	
 	@GetMapping("/{date}")
-	public @ResponseBody ResponseEntity<List<Day>> getCinemasAfterDate(@PathVariable LocalDate date) {
+	public @ResponseBody ResponseEntity<List<Day>> getCinemasAfterDate(@PathVariable @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date) {
 		List<Day> days = planRepo.findDaysAfterDate(date);
 		if(!days.isEmpty()) {
 			return new ResponseEntity<List<Day>>(days,HttpStatus.OK);
