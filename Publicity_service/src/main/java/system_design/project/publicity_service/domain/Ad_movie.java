68a1152d4c6cc471d5caf448a577f500239f4688
jdrbaix <jasper.derbaix@ugent.be>
2019-12-04 22:25:57 +0100

    Begin Publiciteit
    Ik heb gemerkt dat Intellij de persistence (nog) niet herkent, ik zoek naar oplossingen.

---
+++ Publicity_service/src/main/java/system_design/project/publicity_service/domain/Ad_movie.java
@@ -0,0 +1,9 @@
+package main.java.system_design.project.publicity_service.domain;
+
+import java.time.LocalTime;
+
+public class Ad_movie extends AMovie{
+    public Ad_movie(){
+        super();
+    }
+}
---
+++ Publicity_service/src/main/java/system_design/project/publicity_service/domain/Advertisement.java
@@ -0,0 +1,7 @@
+package main.java.system_design.project.publicity_service.domain;
+
+public class Advertisement extends AMovie {
+    public Advertisement(){
+        super();
+    }
+}
