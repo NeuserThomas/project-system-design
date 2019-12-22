7b09054451299447205c7ff462eb0efbb3019d6b
Thomas Neuser <tho.neuser@gmail.com>
2019-12-22 09:21:38 +0100

    Revert "Merge branch 'parking_service'"
    This reverts commit 3dd0e2e75723faa677ea057e0bc361c05f0f4476.

--- parking_service/src/main/java/parking_service/persistence/ParkingRepository.java
+++
@@ -1,9 +0,0 @@
-package parking_service.persistence;
-
-import org.springframework.data.repository.CrudRepository;
-
-import parking_service.domain.Parking;
-
-public interface ParkingRepository extends CrudRepository<Parking, Long> {
-
-}
