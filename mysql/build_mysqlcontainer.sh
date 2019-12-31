89d07c22f66b62540a3da58a3544b4dec99fb10c
rgoussey <robin.goussey@ugent.be>
2019-12-31 15:40:05 +0100

    Working frontend (no link to backend yet)

--- mysql/build_mysqlcontainer.sh
+++ mysql/build_mysqlcontainer.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 #docker network create test_network || true
-docker rm mySQLdb || true
-docker run --name mysqldb -p 3306:3306 --env "MYSQL_ROOT_PASSWORD=ThePassword" --env "MYSQL_DATABASE=HallPlanningService" --env -d mysql:5.7
+docker rm mysqldb || true
+docker run --name mysqldb -p 3306:3306 --env "MYSQL_ROOT_PASSWORD=ThePassword" --env "MYSQL_DATABASE=HallPlanningService" --env "MYSQL_DATABASE=shop" -d mysql:5.7
 #This might make it run faster
 #bash wait-for-mysql.sh mySQLdb -- docker exec -it mySQLdb mysql -p -e "set global innodb_lru_scan_depth=256;"
