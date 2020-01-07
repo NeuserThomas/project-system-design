1c2473e4e28575b8f4f9ef7ee41b4d09581dbc14
rgoussey <robin.goussey@ugent.be>
2020-01-07 13:36:45 +0100

    resolved merge conflicts

--- shop_service/src/main/java/system_design/project/shop_service/adapters/rest/TransactionRestController.java
+++ shop_service/src/main/java/system_design/project/shop_service/adapters/rest/TransactionRestController.java
@@ -2,12 +2,15 @@ package system_design.project.shop_service.adapters.rest;
 
 import java.util.List;
 import java.util.Optional;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -16,6 +19,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.context.request.async.DeferredResult;
+
 import system_design.project.shop_service.domain.Transaction;
 import system_design.project.shop_service.persistence.TransactionRepository;
 import system_design.project.shop_service.service.TransactionService;
@@ -61,12 +66,30 @@ public class TransactionRestController {
 	 * @return
 	 */
 	@PostMapping
-	public ResponseEntity<Transaction> postTransaction(@RequestBody Transaction transaction) {
-		logger.info("postTransaction!");
-		if (transactionService.viableTransaction(transaction)) {
-			return new ResponseEntity<Transaction>(HttpStatus.ACCEPTED);
-		} else {
-			return new ResponseEntity<Transaction>(HttpStatus.CONFLICT);
-		}
+	@Async
+	public DeferredResult<ResponseEntity<Transaction>> postTransaction(@RequestBody Transaction transaction)
+			throws InterruptedException {
+		DeferredResult<ResponseEntity<Transaction>> output = new DeferredResult<>();
+
+		ForkJoinPool.commonPool().submit(() -> {
+			try {
+				if (transactionService.TryAndSell(transaction).get()) {
+					output.setResult(new ResponseEntity<Transaction>(HttpStatus.ACCEPTED));
+				}
+			} catch (InterruptedException | ExecutionException e) {
+				e.printStackTrace();
+			}
+			output.setResult(new ResponseEntity<Transaction>(HttpStatus.CONFLICT));
+		});
+		return output;
 	}
+
+	/*
+	 * public ResponseEntity<Transaction> postTransaction(@RequestBody Transaction
+	 * transaction) { logger.info("postTransaction!");
+	 * if(transactionService.viableTransaction(transaction)) { return new
+	 * ResponseEntity<Transaction>(HttpStatus.ACCEPTED); } else { return new
+	 * ResponseEntity<Transaction>(HttpStatus.CONFLICT); } }
+	 */
+
 }
