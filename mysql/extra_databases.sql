b7068c0748ed3d23b378c828012f99004b51a6cd
Thomas Neuser <tho.neuser@gmail.com>
2020-01-15 18:03:40 +0100

    fixes

--- mysql/extra_databases.sql
+++ mysql/extra_databases.sql
@@ -1,5 +1,6 @@
-CREATE DATABASE IF NOT EXISTS HallPlanningService;
+CREATE DATABASE IF NOT EXISTS HallPlanning;
 CREATE DATABASE IF NOT EXISTS Shop;
 CREATE DATABASE IF NOT EXISTS Ticket;
+CREATE DATABASE IF NOT EXISTS Parking;
 select '--- Creating extra databases ---' AS '';
 SET @@global.time_zone = '+00:00'; = '+1:00';
