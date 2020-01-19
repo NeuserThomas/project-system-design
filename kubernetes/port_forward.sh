44380f3a018e1d62c64b831c3d0b848329c325e8
rgoussey <robin.goussey@ugent.be>
2020-01-19 21:47:59 +0100

    Update script

--- kubernetes/port_forward.sh
+++ kubernetes/port_forward.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-ssh -L 9100:10.244.1.46:80 -i '/home/robin/.jFed/login-certs/cea463f731dbfd13b218d22b2a43129e.pem' rgoussey@n075-06.wall1.ilabt.iminds.be -oPort=22 -oProxyCommand="ssh -i '/home/robin/.jFed/login-certs/cea463f731dbfd13b218d22b2a43129e.pem' -oPort=22 rgoussey@bastion.test.iminds.be -W %h:%p";
+ssh -L 9100:10.107.202.17:80 -i '/home/robin/.jFed/login-certs/cea463f731dbfd13b218d22b2a43129e.pem' rgoussey@n075-06.wall1.ilabt.iminds.be -oPort=22 -oProxyCommand="ssh -i '/home/robin/.jFed/login-certs/cea463f731dbfd13b218d22b2a43129e.pem' -oPort=22 rgoussey@bastion.test.iminds.be -W %h:%p";
